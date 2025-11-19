053db5e074c673383a6ec65d2fc0874d7ed58643
Kim Andersson <Kimpabooy@live.com>
2025-11-19 13:38:05 +0100

    renamed helper file to handeling AJAX for personal desk availability

--- modules/mod_fbg_flexoffice_personal_desk_availability/helper.php
+++ modules/mod_fbg_flexoffice_personal_desk_availability/helper.php
@@ -1,4 +1,7 @@
 <?php
+// Kopia av helper_fbg_flexoffice_personal_desk_availability.php för com_ajax
+// Se till att klassnamnet och metoderna är identiska
+
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
@@ -8,6 +11,13 @@ require_once __DIR__ . '/src/PersonalDeskService.php';
 
 class ModFbgFlexofficePersonalDeskAvailabilityHelper
 {
+    /**
+     * AJAX handler for com_ajax (method=save)
+     */
+    public static function saveAjax()
+    {
+        return PersonalDeskService::handleRequest(true);
+    }
     /**
      * Delegate POST handling to the service layer.
      */
